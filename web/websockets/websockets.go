15d9cd3b172d14eea0de84210b8516773c7d379d
Alexander Antoschuk <alexander.antoschuk.dev@gmail.com>
2025-01-21 14:50:34 +0100

    improved websockets example

--- web/websockets/websockets.go
+++ web/websockets/websockets.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -10,20 +12,54 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize: 1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func (r *http.Request) bool {
+		origin := r.Header.Get("Origin")
+		return "http://localhost:8000" == origin 
+	},
+	// Wait for the WS handshake for no more then 10secs
+	// If the Handshake takes longer, abort the connection attemp
+	HandshakeTimeout: time.Duration(10 * time.Second),
+	EnableCompression: true,
+	// Error  - I can define a custom error here.
 }
 
 func main () {
 	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		conn, _ := upgrader.Upgrade(w, r, nil)
-		for  {
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				return 
-			}
-			fmt.Printf("s% sent: %s\n", conn.RemoteAddr(), string(msg))
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Fatal("Unable to establish connection", err)
+		}
 
-			if err = conn.WriteMessage(msgType, msg); err != nil {
+		// Do not forget to close connection 
+		defer conn.Close()
+
+		// It means after 1 minute from the start connection will be corrupt
+		conn.SetReadDeadline(time.Now().Add(1 * time.Minute))
+
+		inactive := make(chan bool)
+		go func () {
+			<-time.After(1 * time.Minute)
+			inactive <-  true
+		}()
+
+		for  {
+			select {
+			case <-inactive:
+				log.Println("Connection closed du to inactivity")
+				conn.Close()
 				return
+			default:
+				msgType, msg, err := conn.ReadMessage()
+				if err != nil {
+					log.Print("Unable to read the message", err)
+					return
+				}
+				fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+				if err = conn.WriteMessage(msgType, msg); err != nil {
+					log.Print("Unable to send the message", err)
+					return
+				}
 			}
 		}
 	})
@@ -34,3 +70,4 @@ func main () {
 
 	http.ListenAndServe(":8000", nil)
 }
+
