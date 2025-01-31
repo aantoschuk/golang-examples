e26df8c5b13388e9d1611017a15a70f7992a771e
Alexander Antoschuk <alexander.antoschuk.dev@gmail.com>
2025-01-31 03:09:23 +0100

    handle cors

--- web/chat/main.go
+++ web/chat/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+// TODO: learn more about CORS and preflight in golang.
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
 
 func main () {
 	hub := newHub()
 
 	go hub.run()
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
+		fmt.Println("worked")
+		w.Write([]byte("Server Works"))
+
+	})
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWS(hub, w, r)
 	})
