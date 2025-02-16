32d8d7eabc320c2a00b98027d895639c6a001580
Alexander Antoschuk <alexander.antoschuk.dev@gmail.com>
2025-02-16 17:34:57 +0200

    connect to db

--- web/chat/go.mod
+++ web/chat/go.mod
@@ -2,4 +2,7 @@ module example/chat
 
 go 1.23.4
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- web/chat/main.go
+++ web/chat/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
 	"example/chat/api"
 	"example/chat/websocket"
+
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
+func connect() {
+	fmt.Println("Connecting to db")
+	var err error
+	conStr := "postgres://alexander:123456@localhost/chat?sslmode=disable"
+	db, err = sql.Open("postgres", conStr)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("db connected")
+}
+
 func main() {
+	connect()
 	hub := websocket.NewHub()
 
 	go hub.Run()
@@ -26,6 +51,5 @@ func main() {
 	log.Fatal("Listen and Serve: ", err)
 }
 
-// TODO: Improve porject structure
 // FEATURE: add registration
 // FEATURE: Each message should be associated with the user who sent it
